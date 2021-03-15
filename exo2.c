67196894edb386254499513f938569615e002a3e
Helder Lisboa Brites <helder.lisboa-brites@edu.esiee.fr>
2021-03-15 13:54:13 +0100

    exo2 ok

---
+++ exo2.c
@@ -0,0 +1,22 @@
+#include <omp.h>
+#include <stdio.h>
+
+
+int main(int argc, char const *argv[])
+{
+    /**
+     * Programme qui divise en 4 tâches openMP, chacune affichera son numéro de rang
+     * 
+     * */
+    omp_set_num_threads(4);
+    int VALEUR1 = 1000;
+    int VALEUR2 = 2000;
+
+    #pragma omp parallel firstprivate(VALEUR2)
+    {
+        VALEUR2 ++;
+        printf("VALEUR1 = %d\n", VALEUR1);
+        printf("VALEUR2 = %d\n", VALEUR2);
+    }
+    return 0;
+}
